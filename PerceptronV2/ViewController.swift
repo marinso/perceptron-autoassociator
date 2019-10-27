3e20c794f88c005034a2cd20a577e464ce6653ff
Martin Nasierowski <mnasierowski@netkodo.com>
2019-10-27 18:23:48 +0100

    Initial Commit

---
+++ PerceptronV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerceptronV2
+//
+//  Created by Martin Nasierowski on 27/10/2019.
+//  Copyright © 2019 Martin Nasierowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
